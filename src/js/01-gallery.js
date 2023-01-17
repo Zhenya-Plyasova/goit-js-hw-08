cbab0b92ff7619c90b27fe0583d76a1b5006642d
Zhenya Plyasova <111362107+Zhenya-Plyasova@users.noreply.github.com>
2023-01-17 14:21:25 +0200

    first task added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) =>`<a class = "gallery__link" href =${original}><img class = "gallery__image" src="${preview}" alt="${description}"></a>`).join("");
+
+galleryBox.innerHTML=galleryMarkup;
+
+galleryBox.addEventListener("click", showBigPicture);
+function showBigPicture(event){
+    // if (event.target.nodeName !== "IMG"){
+    // return;
+    // }
+    // event.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery a', {
+
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
+
+}
